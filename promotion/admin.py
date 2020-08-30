c3d3cdf73ec7790382b30d7b30d557fa433f2071
wataru_otsuka_0322 <otsuka.wataru.0322@gmail.com>
2020-08-30 20:41:37 +0900

    ポートフォリオ用に特化

--- promotion/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Book, Impression
-
-admin.site.register(Book)
-admin.site.register(Impression)
--- promotion/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PromotionConfig(AppConfig):
-    name = 'promotion'
