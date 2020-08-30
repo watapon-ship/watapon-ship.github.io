3b165aa1b28ed62044768e0af884f12138887f18
wataru_otsuka_0322 <otsuka.wataru.0322@gmail.com>
2020-08-30 19:10:36 +0900

    appを追加

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'promotion.apps.PromotionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
